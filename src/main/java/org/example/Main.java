a436d7d34074aaec5562e4c0d29755544fe1e5d9
Danil Romanov <danillyou@mail.ru>
2025-03-19 12:40:01 +0300

    Done

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        ListImpl <Integer> set = new ListImpl<Integer>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(5);
+        set.remove(2);
+        System.out.println(set);
+    }
+}
